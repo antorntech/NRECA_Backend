3626e108538296ea2521dd98003a28720adddd1c
MD Abdul Motaleb Antor <antor@piit.local>
2024-07-11 16:17:00 +0600

    add some change

--- models/Leaves.js
+++ models/Leaves.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const leavesSchema = new mongoose.Schema(
   {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
+    email: {
+      type: String,
     },
     leaveCategory: {
       type: String,
@@ -12,6 +12,9 @@ const leavesSchema = new mongoose.Schema(
       type: String,
     },
     date: {
+      type: String,
+    },
+    days: {
       type: String,
     },
     reason: {
