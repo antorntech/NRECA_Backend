154e4c5aa9ffd7a31f845d7a1c19ac5990b24ba6
antor10655gh <creativefreelancerantor@gmail.com>
2024-07-15 14:40:57 +0600

    add some change on employee

--- models/Employee.js
+++ models/Employee.js
@@ -1,73 +1,155 @@
 const mongoose = require("mongoose");
 
-const employeeSchema = new mongoose.Schema(
-  {
-    officeId: {
-      type: Number,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-    },
-    nationality: {
-      type: String,
-    },
-    dob: {
-      type: String,
-    },
-    employeeType: {
-      type: String,
-    },
-    joiningDate: {
-      type: String,
-    },
-    endDate: {
-      type: String,
-      default: "Running",
-    },
-    projectName: {
-      type: String,
-    },
-    department: {
-      type: String,
-    },
-    designation: {
-      type: String,
-    },
-    officeEmail: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-    },
-    avatar: {
-      type: String,
-      default: "N/A",
-    },
-    casualLeave: {
-      type: Number,
-      default: 10,
-    },
-    sickLeave: {
-      type: Number,
-      default: 10,
-    },
-    employeeType: {
-      type: String,
-      default: "Full Time",
-    },
-    employeeStatus: {
-      type: String,
-      default: "Active",
-    },
+const employeeSchema = new mongoose.Schema({
+  // Offine Information
+  officeId: {
+    type: Number,
+    default: 101,
   },
-  {
-    timestamps: true,
-  }
-);
+  name: {
+    type: String,
+    default: "N/A",
+  },
+  gender: {
+    type: String,
+    default: "N/A",
+  },
+  nationality: {
+    type: String,
+    default: "Bangladeshi",
+  },
+  dob: {
+    type: String,
+    default: "N/A",
+  },
+  employeeType: {
+    type: String,
+    default: "Full Time",
+  },
+  joiningDate: {
+    type: String,
+    default: "N/A",
+  },
+  endDate: {
+    type: String,
+    default: "Running",
+  },
+  projectName: {
+    type: String,
+    default: "N/A",
+  },
+  department: {
+    type: String,
+    default: "N/A",
+  },
+  designation: {
+    type: String,
+    default: "N/A",
+  },
+  officeEmail: {
+    type: String,
+    default: "N/A",
+  },
+  phoneNumber: {
+    type: String,
+    default: "1234567890",
+  },
+  avatar: {
+    type: String,
+    default: "N/A",
+  },
+  casualLeave: {
+    type: Number,
+    default: 10,
+  },
+  sickLeave: {
+    type: Number,
+    default: 10,
+  },
+  employeeType: {
+    type: String,
+    default: "Full Time",
+  },
+  employeeStatus: {
+    type: String,
+    default: "Active",
+  },
+
+  // Personal Information
+  personalPhoneNumber: {
+    type: String,
+    default: "1234567890",
+  },
+  personalEmail: {
+    type: String,
+    default: "N/A",
+  },
+  presentAddress: {
+    type: String,
+    default: "N/A",
+  },
+  permanentAddress: {
+    type: String,
+    default: "N/A",
+  },
+  nid: {
+    type: String,
+    default: "N/A",
+  },
+  nidDoc: {
+    type: String,
+  },
+  passport: {
+    type: String,
+    default: "N/A",
+  },
+  passportDoc: {
+    type: String,
+  },
+  tin: {
+    type: String,
+    default: "N/A",
+  },
+  tinDoc: {
+    type: String,
+  },
+  singnatureDoc: {
+    type: String,
+  },
+  emergencyNumber: {
+    type: String,
+    default: "1234567890",
+  },
+  relationShipNumber: {
+    type: String,
+    default: "1234567890",
+  },
+
+  // Bank Information
+  bankName: {
+    type: String,
+    default: "N/A",
+  },
+  bankBranch: {
+    type: String,
+    default: "N/A",
+  },
+  accountNumber: {
+    type: String,
+    default: "X1215000012500020001",
+  },
+  routingNumber: {
+    type: String,
+    default: "X5484587",
+  },
+  accountTitle: {
+    type: String,
+    default: "Mr. Xyz",
+  },
+  accountEmail: {
+    type: String,
+    default: "N/A",
+  },
+});
 
 module.exports = mongoose.model("Employee", employeeSchema, "Employee");
